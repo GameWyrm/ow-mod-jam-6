eae4df9094dd9886ef8db8d9f7942ac0a1c97a98
GameWyrm <daronan97@hotmail.com>
2026-07-31 21:49:14 -0700

    Initial commit

---
+++ Mod Jam 6/Mod Jam 6.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace Mod Jam 6;
+public class Mod Jam 6 : ModBehaviour
+{
+	public static Mod Jam 6 Instance;
+	public INewHorizons NewHorizons;
+
+public void Awake()
+{
+    Instance = this;
+    // You won't be able to access OWML's mod helper in Awake.
+    // So you probably don't want to do anything here.
+    // Use Start() instead.
+}
+
+public void Start()
+{
+    // Starting here, you'll have access to OWML's mod helper.
+    ModHelper.Console.WriteLine($"My mod {nameof(Mod Jam 6)} is loaded!", MessageType.Success);
+
+    // Get the New Horizons API and load configs
+    NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+    NewHorizons.LoadConfigs(this);
+
+    new Harmony("GameWyrm,pikpik,Stache,andCallirhoe.Mod Jam 6").PatchAll(Assembly.GetExecutingAssembly());
+
+    // Example of accessing game code.
+    OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+    LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+}
+
+public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+{
+    if (newScene != OWScene.SolarSystem) return;
+    ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+}
+}
+
