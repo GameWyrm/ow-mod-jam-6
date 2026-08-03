723d4f433a9f80a76bb305857e539164ad321779
GameWyrm <daronan97@hotmail.com>
2026-08-02 22:51:51 -0700

    Time bubble

---
+++ Mod Jam 6/MaterialManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Mod_Jam_6
+{
+    public class MaterialManager : MonoBehaviour
+    {
+        public Material[] materials;
+
+        public void UpdateMaterials()
+        {
+            foreach (Material mat in materials)
+            {
+                mat.SetFloat("_TimeProgress", 1); // TODO set this to the percentage of time passed
+                mat.SetVector("_BubblePosition", TimeBubble.instance.gameObject.transform.position);
+                mat.SetFloat("_BubbleDistance", TimeBubble.instance.size);
+            }
+        }
+
+        private void Update()
+        {
+            UpdateMaterials();
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            UpdateMaterials();
+        }
+    }
+}
