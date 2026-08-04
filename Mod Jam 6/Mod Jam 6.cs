f31beff13b66bc935a733fd9af916a62de6ddbef
Echatsum <Echatsum@protonmail.com>
2026-08-04 02:57:09 +0200

    mini file changes because angry error on namespace

--- Mod Jam 6/Mod Jam 6.cs
+++ Mod Jam 6/Mod Jam 6.cs
@@ -3,40 +3,42 @@ using OWML.Common;
 using OWML.ModHelper;
 using System.Reflection;
 
-namespace Mod_Jam_6;
-public class ModJam6 : ModBehaviour
+namespace Mod_Jam_6
 {
-	public static ModJam6 Instance;
-	public INewHorizons NewHorizons;
+    public class ModJam6 : ModBehaviour
+    {
+        public static ModJam6 Instance;
+        public INewHorizons NewHorizons;
 
-public void Awake()
-{
-    Instance = this;
-    // You won't be able to access OWML's mod helper in Awake.
-    // So you probably don't want to do anything here.
-    // Use Start() instead.
-}
+        public void Awake()
+        {
+            Instance = this;
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
 
-public void Start()
-{
-    // Starting here, you'll have access to OWML's mod helper.
-    ModHelper.Console.WriteLine($"My mod {nameof(ModJam6)} is loaded!", MessageType.Success);
+        public void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(ModJam6)} is loaded!", MessageType.Success);
 
-    // Get the New Horizons API and load configs
-    NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
-    NewHorizons.LoadConfigs(this);
+            // Get the New Horizons API and load configs
+            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizons.LoadConfigs(this);
 
-    new Harmony("GameWyrm.Mod Jam 6").PatchAll(Assembly.GetExecutingAssembly());
+            new Harmony("GameWyrm.Mod Jam 6").PatchAll(Assembly.GetExecutingAssembly());
 
-    // Example of accessing game code.
-    OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
-    LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
-}
+            // Example of accessing game code.
+            OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+        }
 
-public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
-{
-    if (newScene != OWScene.SolarSystem) return;
-    ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-}
+        public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+        {
+            if (newScene != OWScene.SolarSystem) return;
+            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        }
+    }
 }
 
