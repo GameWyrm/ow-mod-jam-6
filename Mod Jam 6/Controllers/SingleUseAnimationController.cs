8ea7659b454c2e136e4b805725af06a5beb8a2f0
Echatsum <Echatsum@protonmail.com>
2026-08-04 11:54:04 +0200

    Single-use interact button and animation controller. (untested)

---
+++ Mod Jam 6/Controllers/SingleUseAnimationController.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+namespace Mod_Jam_6.Controllers
+{
+    // Base of an animation controller with virtual methods. create another class and override the PlayAnim() for more custom animations
+    public class SingleUseAnimationController : MonoBehaviour
+    {
+        [SerializeField]
+        private SingleUseInteract _singleUseInteract;
+
+        [SerializeField]
+        private Animator _mainAnimator;
+        [SerializeField]
+        private OWAudioSource _mainAudio;
+
+        protected virtual void Start()
+        {
+            if (_singleUseInteract != null)
+            {
+                _singleUseInteract.OnSingleUseActivation += OnActivation;
+            }
+        }
+        protected virtual void OnDestroy()
+        {
+            if (_singleUseInteract != null)
+            {
+                _singleUseInteract.OnSingleUseActivation -= OnActivation;
+            }
+        }
+
+        protected virtual void OnActivation()
+        {
+            StartCoroutine(PlayAnim());
+        }
+
+        protected virtual IEnumerator PlayAnim()
+        {
+            _mainAudio?.PlayOneShot(global::AudioType.NomaiVesselPowerUp, 1f);
+            _mainAnimator?.Play("ACTIVATION", 0);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
