f7767bde1b06956bfe498fb954bbc62ce2ce00be
GameWyrm <daronan97@hotmail.com>
2026-08-03 18:01:19 -0700

    More model stuff

--- Mod Jam 6/MaterialManager.cs
+++ Mod Jam 6/MaterialManager.cs
@@ -5,12 +5,16 @@ namespace Mod_Jam_6
     public class MaterialManager : MonoBehaviour
     {
         public Material[] materials;
+        public float timeProgress = 0.25f;
+
+        private float minProgress = 0.15f;
+        private float maxProgress = 0.95f;
 
         public void UpdateMaterials()
         {
             foreach (Material mat in materials)
             {
-                mat.SetFloat("_TimeProgress", 0.1f); // TODO set this to the percentage of time passed
+                mat.SetFloat("_TimeProgress", timeProgress); // TODO set this to the percentage of time passed
                 mat.SetVector("_BubblePosition", TimeBubble.instance.gameObject.transform.position);
                 mat.SetFloat("_BubbleDistance", TimeBubble.instance.size);
             }
