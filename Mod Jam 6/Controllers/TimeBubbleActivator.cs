5202af6995547c2923d8af3c67c06edd2bd21b04
Echatsum <Echatsum@protonmail.com>
2026-08-04 03:00:35 +0200

    Time bubble activator (first draft, untested, some edge cases to do)

---
+++ Mod Jam 6/Controllers/TimeBubbleActivator.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+namespace Mod_Jam_6
+{
+    public class TimeBubbleActivator : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _timeBubbleLocation;
+        [SerializeField]
+        private float _timeBubbleSize;
+        [SerializeField]
+        private float _timeBubbleExpansionDuration; // How fast the bubble will expand to its max size
+        [SerializeField]
+        private float _timeBubbleCollapseDuration; // How fast the bubble will collapse (assuming from its max size)
+
+        [SerializeField]
+        private bool _isActive;
+        public bool IsActive => _isActive;
+
+        public void SetActive(bool isActive)
+        {
+            if (_isActive == isActive) return;
+
+            _isActive = isActive;
+            if (isActive)
+            {
+                TimeBubble.instance.TryActivate(_timeBubbleLocation.position, _timeBubbleSize, _timeBubbleExpansionDuration, _timeBubbleCollapseDuration);
+            }
+            else
+            {
+                TimeBubble.instance.Deactivate();
+            }
+        }
+    }
+}
--- Mod Jam 6/TimeBubble.cs
+++ Mod Jam 6/TimeBubble.cs
@@ -6,16 +6,122 @@ namespace Mod_Jam_6
     {
         public static TimeBubble instance;
 
-        public float size;
+        private float _size;
+        public float size => _size;
+
+        private bool _isShrunk;
+        private bool _isExpanded;
+        private bool _isExpanding;
+        private bool _isShrinking;
+        private bool _wishToExpand;
+
+        private Vector3 _nextPosition;
+        private float _nextExpandedSize;
+        private float _nextExpansionDuration;
+        private float _nextCollapseDuration;
+        private float _currentExpandedSize;
+        private float _currentExpansionDuration;
+        private float _currentCollapseDuration;
+
+        private float _lerpTimeStart;
+        private float _lerpTimeCurrent;
+
 
         private void Start()
         {
             instance = this;
+
+            _size = 0f;
+            _isShrunk = true;
         }
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.cyan;
-            Gizmos.DrawWireSphere(transform.position, size);
+            Gizmos.DrawWireSphere(transform.position, _size);
+        }
+
+        public void TryActivate(Vector3 newPosition, float newExpandedSize, float newExpansionDuration, float newCollapseDuration)
+        {
+            // Case 1: Activating where the bubble already is. This is simple, we just tell it to expand.
+            if(transform.position == newPosition)
+            {
+                if (!_isExpanded) // [Stache TODO]: I am aware it currently does not cover all edge cases, I will do it later
+                {
+                    _currentExpandedSize = newExpandedSize;
+                    _currentExpansionDuration = newExpansionDuration;
+                    _currentCollapseDuration = newCollapseDuration;
+
+                    _isExpanding = true;                                        
+                    float ratio = _size / newExpandedSize;
+                    float equivalentLerpingTimeElapsed = ratio * newExpansionDuration;
+                    _lerpTimeStart = TimeLoop.GetSecondsElapsed() - equivalentLerpingTimeElapsed;
+                }
+                return;
+            }
+
+            // Case 2: Different place. Here we need to shrink the potential current one first, then move it.
+            _nextPosition = newPosition;
+            _nextExpandedSize = newExpandedSize;
+            _nextExpansionDuration = newExpansionDuration;
+            _nextCollapseDuration = newCollapseDuration;
+
+            if (!_isShrunk) {
+                _isShrinking = true;
+                float ratio = _size / newExpandedSize;
+                float equivalentLerpingTimeElapsed = ratio * newCollapseDuration;
+                _lerpTimeStart = TimeLoop.GetSecondsElapsed() - equivalentLerpingTimeElapsed;
+            }
+            _wishToExpand = true;
+        }
+        public void Deactivate()
+        {
+            if (_isShrunk) return;
+
+            _wishToExpand = false;
+            _isExpanding = false;
+            _isExpanded = false;
+            _isShrinking = true;
+            float ratio = _size / _currentExpandedSize;
+            float equivalentLerpingTimeElapsed = ratio * _currentCollapseDuration;
+            _lerpTimeStart = TimeLoop.GetSecondsElapsed() - equivalentLerpingTimeElapsed;
+        }
+
+        public void Update()
+        {
+            if (_isShrinking)
+            {
+                _lerpTimeCurrent = (TimeLoop.GetSecondsElapsed() - _lerpTimeStart) / _currentCollapseDuration;
+                _size = Mathf.Lerp(_currentExpandedSize, 0f, _lerpTimeCurrent);
+
+                if(_size <= 0f)
+                {
+                    _isShrinking = false;
+                    _isShrunk = true;
+                }
+            }
+            else if (_isExpanding)
+            {
+                _lerpTimeCurrent = (TimeLoop.GetSecondsElapsed() - _lerpTimeStart) / _currentExpansionDuration;
+                _size = Mathf.Lerp(0f, _currentExpandedSize, _lerpTimeCurrent);
+
+                if (_size >= _currentExpandedSize)
+                {
+                    _isExpanding = false;
+                    _isExpanded = true;
+                }
+            }
+            else if (_wishToExpand)
+            {
+                transform.position = _nextPosition;
+                _currentExpandedSize = _nextExpandedSize;
+                _currentExpansionDuration = _nextExpansionDuration;
+                _currentCollapseDuration = _nextCollapseDuration;
+
+                _lerpTimeStart = TimeLoop.GetSecondsElapsed();
+
+                _isExpanding = true;
+                _wishToExpand = false;
+            }
         }
     }
 }
