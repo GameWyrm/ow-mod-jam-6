83f386c52b079d7a24c8f4195900b547f300d34e
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2026-08-04 20:23:56 -0500

    added basic shiplog teleport, does nothing rn tho

---
+++ Mod Jam 6/TeleportShiplog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Mod_Jam_6
+{
+    public class TeleportShiplog : MonoBehaviour
+    {
+        [SerializeField] GameObject targetLocation;
+        //in EotP i have a set object thats refrenced here, but it could be done diffrentlly
+        [SerializeField] GameObject shipLog;
+
+        private void Start()
+        {
+            GetComponent<OWTriggerVolume>().OnEntry += Teleport;
+        }
+
+        private void Teleport(GameObject hitObject)
+        {
+            if (hitObject.CompareTag("PlayerDetector"))
+            {
+                StartCoroutine(TeleportWait());
+            }
+        }
+
+        private IEnumerator TeleportWait()
+        {
+            yield return new WaitForSeconds(3);
+            ModJam6.Instance.ModHelper.Console.WriteLine("Moving ship log (supposedly)", OWML.Common.MessageType.Info);
+            shipLog.transform.position = targetLocation.transform.position;
+            shipLog.transform.rotation = targetLocation.transform.rotation;
+        }
+    }
+}
