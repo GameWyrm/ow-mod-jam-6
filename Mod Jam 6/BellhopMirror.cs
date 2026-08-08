7006d756db6086b9f33884e8b71bed646229d4db
Echatsum <Echatsum@protonmail.com>
2026-08-09 00:19:57 +0200

    bellhop mirror / Lobby door (pseudo quantum way)

---
+++ Mod Jam 6/BellhopMirror.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod_Jam_6
+{
+    public class BellhopMirror : QuantumObject
+    {
+        public override bool ChangeQuantumState(bool skipInstantVisibilityCheck)
+        {
+            return false; // Does not actually move
+        }
+    }
+}
---
+++ Mod Jam 6/Controllers/Triggers/LobbyDoorTrigger.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+namespace Mod_Jam_6.Controllers
+{
+    public class LobbyDoorTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private BellhopMirror _bellhopMirror;
+
+        [SerializeField]
+        private GameObject[] _objectsToToggleOn;
+        [SerializeField]
+        private GameObject[] _objectsToToggleOff;
+
+        private bool _hasBeenSolved = false;
+
+        public void OnTriggerEnter(Collider hitCollider)
+        {
+            if(_hasBeenSolved) { return; }
+
+            if (hitCollider.CompareTag("PlayerDetector") && _bellhopMirror.IsLockedByActiveCamera())
+            {
+                foreach (var obj in _objectsToToggleOff)
+                {
+                    obj.SetActive(false);
+                }
+                foreach (var obj in _objectsToToggleOn)
+                {
+                    obj.SetActive(true);
+                }
+                _hasBeenSolved = true;
+            }
+        }
+    }
+}
