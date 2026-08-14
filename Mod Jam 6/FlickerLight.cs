6ae02c9024b4f9db5aeaa2ac75e87396095381aa
GameWyrm <daronan97@hotmail.com>
2026-08-13 21:29:18 -0700

    Flicker light

---
+++ Mod Jam 6/FlickerLight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Mod_Jam_6
+{
+    public class FlickerLight : MonoBehaviour
+    {
+        public float maxTime = 1;
+        public float minTime = 0.1f;
+        public GameObject light;
+        public Material litMaterial;
+        public Material unlitMaterial;
+        public MeshRenderer rend;
+
+        private float waitTime;
+        private bool lit = true;
+
+        private void Start ()
+        {
+            waitTime = Random.Range(minTime, maxTime);
+            StartCoroutine(Flicker());
+        }
+
+        private void OnDestroy()
+        {
+            StopAllCoroutines();
+        }
+
+        private IEnumerator Flicker()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(waitTime);
+                lit = !lit;
+                light.SetActive(lit);
+                if (lit)
+                {
+                    rend.material = litMaterial;
+                }
+                else
+                {
+                    rend.material = unlitMaterial;
+                }
+                waitTime = Random.Range(minTime, maxTime);
+            }
+        }
+    }
+}
