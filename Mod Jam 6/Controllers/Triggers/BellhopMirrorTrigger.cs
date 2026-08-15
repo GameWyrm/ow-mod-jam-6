e3d19874a6bf487b22b266809f72322d5a0be2ae
Echatsum <Echatsum@protonmail.com>
2026-08-15 03:13:02 +0200

    bellhop mirror debug

--- Mod Jam 6/Controllers/Triggers/BellhopMirrorTrigger.cs
+++ Mod Jam 6/Controllers/Triggers/BellhopMirrorTrigger.cs
@@ -20,6 +20,7 @@ namespace Mod_Jam_6
         {
             if (hitCollider.CompareTag("PlayerDetector"))
             {
+                ModJam6.Instance.ModHelper.Console.WriteLine("ENTERED BELLHOP ROOM", OWML.Common.MessageType.Error);
                 ToggleObjects(isEntry: true);
             }
         }
@@ -28,6 +29,7 @@ namespace Mod_Jam_6
             if (hitCollider.CompareTag("PlayerDetector"))
             {
                 base.enabled = true;
+                _isSolving = true;
             }
         }
 
@@ -35,20 +37,25 @@ namespace Mod_Jam_6
         public void SetSolved()
         {
             _isSolved = true;
+            _isSolving = false;
         }
 
         private void Update()
         {
             if (_isSolved)
             {
+                ModJam6.Instance.ModHelper.Console.WriteLine("MIRROR IS SOLVED", OWML.Common.MessageType.Error);
                 base.enabled = false;
+                _isSolving = false;
                 return;
             }
 
             if (!_bellhopMirror.IsLockedByProbeSnapshot()) // Toggle back if photo no longer held
             {
+                ModJam6.Instance.ModHelper.Console.WriteLine("STOPPED LOOKING AT PIC", OWML.Common.MessageType.Error);
                 ToggleObjects(isEntry: false);
                 base.enabled = false;
+                _isSolving = false;
             }
         }
 
@@ -56,6 +63,7 @@ namespace Mod_Jam_6
         {
             if (_objectsToToggleOn != null)
             {
+                ModJam6.Instance.ModHelper.Console.WriteLine($"TURNING {_objectsToToggleOn.Length} OBJECTS ON {isEntry}", OWML.Common.MessageType.Error);
                 foreach (var obj in _objectsToToggleOn)
                 {
                     obj.SetActive(isEntry); // on entry, toggle ON
@@ -63,6 +71,7 @@ namespace Mod_Jam_6
             }
             if (_objectsToToggleOff != null)
             {
+                ModJam6.Instance.ModHelper.Console.WriteLine($"TURNING {_objectsToToggleOff.Length} OBJECTS OFF {isEntry}", OWML.Common.MessageType.Error);
                 foreach (var obj in _objectsToToggleOff)
                 {
                     obj.SetActive(!isEntry); // on entry, toggle OFF
