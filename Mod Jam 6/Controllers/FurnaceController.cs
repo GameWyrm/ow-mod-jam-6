70d5c04c97dd585e5549c959b7230e39005ba6a6
GameWyrm <daronan97@hotmail.com>
2026-08-14 18:06:29 -0700

    Imprisoned in ice forever

--- Mod Jam 6/Controllers/FurnaceController.cs
+++ Mod Jam 6/Controllers/FurnaceController.cs
@@ -5,19 +5,23 @@ namespace Mod_Jam_6
     public class FurnaceController : MonoBehaviour
     {
         public static FurnaceController Instance;
+        public static FurnaceController FakeInstance;
 
         public int floor; //0-3
         public Animator anim;
+        public bool realInstance = false;
 
         private void Start ()
         {
-            Instance = this;
+            if (realInstance) Instance = this;
+            else FakeInstance = this;
         }
 
         public void TurnValve(bool clockwise)
         {
             floor = Mathf.Clamp(floor + (clockwise ? 1 : -1), 0, 3);
             anim.SetInteger("CrankValue", floor);
+            FakeInstance.anim.SetInteger("CrankValue", floor);
         }
     }
 }
---
+++ Mod Jam 6/Controllers/IceController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Mod_Jam_6
+{
+    public class IceController : MonoBehaviour
+    {
+        public CapsuleCollider talkVolume;
+        public Animator anim;
+
+        private void Start()
+        {
+            talkVolume.enabled = false;
+            TimeManager.instance.timeEvents[3].AddListener(TryMelt);
+        }
+
+        private void TryMelt()
+        {
+            if (FurnaceController.Instance.floor == 3)
+            {
+                talkVolume.enabled = true;
+                anim.SetTrigger("Melt");
+            }
+        }
+    }
+}
