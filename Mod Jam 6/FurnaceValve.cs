e5a0ea4cfa5965cef3ca48cee2cbcaac6a1646ce
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2026-08-14 21:43:55 -0500

    added signal goobler

--- Mod Jam 6/FurnaceValve.cs
+++ Mod Jam 6/FurnaceValve.cs
@@ -12,7 +12,7 @@ namespace Mod_Jam_6
         {
             interact = this.GetRequiredComponent<SingleInteractionVolume>();
             interact.OnPressInteract += OnPressInteract;
-            interact.ChangePrompt(ModJam6.Instance.NewHorizons.GetTranslationForUI(clockwise ? "$PH_VALVE_CLOCKWISE" : "$PH_VALVE_COUNTERCLOCKWISE"));
+            interact.ChangePrompt(ModJam6.NewHorizons.GetTranslationForUI(clockwise ? "$PH_VALVE_CLOCKWISE" : "$PH_VALVE_COUNTERCLOCKWISE"));
         }
 
         private void OnPressInteract()
