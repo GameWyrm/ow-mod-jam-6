56b8794e9543738f10ec4b7f701d29bd092ce44d
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2026-08-17 15:26:51 -0500

    flashback code

--- Mod Jam 6/Controllers/RiggedATPPatch.cs
+++ Mod Jam 6/Controllers/RiggedATPPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace Mod_Jam_6
 {
@@ -79,6 +80,65 @@ namespace Mod_Jam_6
             if (!PlayerData.KnowsSignal(name))
             {
                 PlayerData.LearnSignal(name);
+            }
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Flashback), nameof(Flashback.OnTriggerFlashback))]
+        public static bool OnTriggerFlashback(Flashback __instance)
+        {
+            if (ModJam6.NewHorizons.GetCurrentStarSystem() == "VoidDimension")
+            {
+                if (PlayerData.GetPersistentCondition("GAME_OVER_LAST_SAVE"))
+                {
+                    PlayerData.SetPersistentCondition("GAME_OVER_LAST_SAVE", false);
+                }
+                CenterOfTheUniverse.DeactivateUniverse();
+                Locator.GetActiveCamera().enabled = false;
+                __instance.transform.position = Vector3.zero;
+                __instance.ResetEffects();
+                __instance._flashbackCamera.clearFlags = CameraClearFlags.Color;
+                __instance._flashbackCamera.enabled = true;
+                GlobalMessenger<OWCamera>.FireEvent("SwitchActiveCamera", __instance._flashbackCamera);
+                __instance._audioListener.enabled = true;
+                __instance._screenTransform.gameObject.SetActive(true);
+                __instance._maskTransform.gameObject.SetActive(true);
+                __instance._forwardStreams.SetActive(true);
+                int numSnapshots = __instance._flashbackRecorder.GetNumSnapshots();
+                float num = 0f;
+                __instance._imageDisplayTimes = new float[numSnapshots];
+                for (int i = 0; i < numSnapshots; i++)
+                {
+                    num += Mathf.Max(0.6f * Mathf.Pow(0.9f, (float)i), 0.06f);
+                    __instance._imageDisplayTimes[numSnapshots - 1 - i] = num;
+                }
+                __instance._snapshotIndex = numSnapshots - 1;
+                num += 1f;
+                /**
+                DeathType deathType = Locator.GetDeathManager().GetDeathType();
+                if (deathType == DeathType.Supernova || deathType == DeathType.Energy || deathType == DeathType.Lava || deathType == DeathType.DreamExplosion)
+                {
+                    __instance._flashbackStartDelay = 3f;
+                    __instance._whiteFadeAnimator.gameObject.SetActive(true);
+                    __instance._whiteFadeAnimator.SetImmediate(1f);
+                    __instance._whiteFadeAnimator.AnimateTo(0f, Vector3.one, 3f, __instance._whiteFadeCurve, false);
+                }
+                **/
+                GameObject.Find("FlashbackCamera/Mask").SetActive(false);
+                __instance._flashbackStartDelay = 2f;
+                GameObject.Find("HelmentRoot").GetComponent<Animator>().Play("MaskStart");
+                __instance._flashbackTimer = new Timer(__instance._flashbackStartDelay + __instance._playbackDelay + num);
+                __instance._updateFlashback = true;
+                __instance._screenTransform.SetLocalPositionZ(__instance._screenStartDist);
+                __instance._screenRenderer.material.color = new Color(1f, 1f, 1f, 0f);
+                __instance.SetFlashbackImage(__instance._snapshotIndex);
+                __instance._maskTransform.SetLocalPositionZ(__instance._maskStartDist);
+                GlobalMessenger.FireEvent("FlashbackStart");
+                LoadManager.ReloadSceneAsync(false, false);
+                return false;
+            }
+            else
+            {
+                return true;
             }
         }
     }
